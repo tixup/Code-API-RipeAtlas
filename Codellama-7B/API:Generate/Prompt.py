eb1f3872fe06ca2284df0a822aee0a7ab0e138a1
Matteo Tognetti <104846981+tixup@users.noreply.github.com>
2025-06-11 10:53:23 +0200

    Update Prompt.py

--- Codellama-7B/API:Generate/Prompt.py
+++ Codellama-7B/API:Generate/Prompt.py
@@ -3,7 +3,7 @@ import requests
 prompt = """ Hi, what's up? """
 
 response = requests.post("http://localhost:11434/api/generate", json={
-    "model": "codellama",
+    "model": "codellama",    # inserire il modello scelto
     "prompt": prompt,
     "stream": False
 })
