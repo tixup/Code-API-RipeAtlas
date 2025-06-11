bbbc3a46f226f013348e326818a5a9242cfd3f6d
Matteo Tognetti <104846981+tixup@users.noreply.github.com>
2025-06-11 10:54:49 +0200

    Update Prompt.py

--- DeepSeeker-coder:6.7B/API:Generate/Prompt.py
+++ DeepSeeker-coder:6.7B/API:Generate/Prompt.py
@@ -3,7 +3,7 @@ import requests
 prompt = """ Hi, what's up? """
 
 response = requests.post("http://localhost:11434/api/generate", json={
-    "model": "deepseek-coder:6.7b",
+    "model": "deepseek-coder:6.7b",    # inserire modello scelto
     "prompt": prompt,
     "stream": False
 })
